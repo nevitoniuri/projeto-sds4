00260ad1b27296e6e49aa4c703c1d30ab7c1aab9
Iuri Néviton <iurineviton@gmail.com>
2021-09-09 16:56:03 -0300

    footer att

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,10 +1,30 @@
+import imgLinkedin from 'assets/img/linkedin-original.svg';
+import imgGithub from 'assets/img/github-original.svg'
+
 function Footer() {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
+
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/nevitoniuri" target="_blank" rel="noreferrer">Iuri Néviton</a></p>
-                <p className="text-light"><small><strong>Semana Spring React</strong><br />
-                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                <p className="text-light">
+                    <div className="my-2 my-md-0">
+                        App desenvolvido por <strong> Iuri Néviton </strong>
+                    </div>
+                    <div >
+                        <a href="https://www.linkedin.com/in/nevitoniuri/" target="_blank" rel="noreferrer" id="nomeRodape">
+                            <img src={imgLinkedin} alt="DevSuperior" width="30" />
+                        </a>
+
+                        <a href="https://github.com/nevitoniuri/projeto-sds4" target="_blank" rel="noreferrer" id="nomeRodape">
+                            <img src={imgGithub} alt="DevSuperior" width="35" />
+                        </a>
+                    </div>
+                </p>
+                <div className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior:
+                    <a href="https://instagram.com/devsuperior.ig" target="_blank"
+                        rel="noreferrer">@devsuperior.ig</a></small>
+                </div>
             </div>
         </footer>
     );
